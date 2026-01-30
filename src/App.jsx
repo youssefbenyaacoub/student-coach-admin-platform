73de0d13e3592ad093972a106ab137c62b05c8c0
youssefbenyaacoub <youssef.benyaacoub@esen.tn>
2026-01-30 21:17:02 +0100

    forum 4.1.2

--- src/App.jsx
+++ src/App.jsx
@@ -139,9 +139,9 @@ export default function App() {
         <Route path="forum">
           <Route index element={<ForumHome />} />
           <Route path="category/:categoryId" element={<CategoryView />} />
-          <Route path="calendar" element={<CalendarPage />} />
           <Route path="topic/:topicId" element={<TopicView />} />
         </Route>
+        <Route path="calendar" element={<CalendarPage />} />
       </Route>
 
       <Route path="*" element={<NotFound />} />
